4bcbe978d8e2eaf3b1896ea945e83c3823ba21c3
msmart48 <71365762+msmart48@users.noreply.github.com>
2024-04-30 13:06:49 +0930

    Changes to diary

--- 2024 Year 9 Project Examples/diary/script.js
+++ 2024 Year 9 Project Examples/diary/script.js
@@ -1,21 +1,51 @@
 const jobNameInput = document.getElementById("jobNameInput");
 const dueDateInput = document.getElementById("dueDateInput");
 const catInput = document.getElementById("catInput");
+const taskOutput = document.getElementById("taskOutput");
 
-var jobs = [];
+var tasks = [];
 
 //get job details and add to jobs array
 function addTask() {
-    var job = jobNameInput.value;
+    var task = jobNameInput.value;
     var date = dueDateInput.value;
 
     var category = catInput.value;
-    jobs.push(
+    tasks.push(
         {
-            task: job,
-            date: date,
+            task: task,
+            date: Date.parse(date),
             category: category
         }
     )
-    //
+    //convert date object to formatted date
+    console.log(new Date(date).toLocaleDateString('en-au', { weekday: "long", year: "numeric", month: "short", day: "numeric" }))
+
+    selectTasks('all');
 }
+
+//this function will create a duplicate task list ready to print to screen based on users needs and order options.
+function selectTasks(type) {
+    if (type == "all") {
+        //sort from earliest to latest
+        var list = tasks.sort((a, b) => a.date - b.date);
+    }
+
+    //send list the to display list function
+    showTasks(list);
+}
+
+//send a date to format into common format for reading
+function formatDate(date) {
+    var formattedDate = new Date(date).toLocaleDateString('en-au', { weekday: "long", year: "numeric", month: "short", day: "numeric" });
+    return formattedDate;
+}
+
+function showTasks(list) {
+    var text = "";
+    for (var i = 0; i < list.length; i++) {
+        text += `<div class="box">${formatDate(list[i].date)}<br>${list[i].task}<br>${list[i].category}</div>`;
+    }
+    taskOutput.innerHTML = text;
+}
+
