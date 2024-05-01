4f82fffd1f5df4bc9cbe99775ddec5537ec058f1
msmart48 <71365762+msmart48@users.noreply.github.com>
2024-05-01 12:42:45 +0930

    Add and remove items

--- 2024 Year 9 Project Examples/diary/script.js
+++ 2024 Year 9 Project Examples/diary/script.js
@@ -13,23 +13,42 @@ function addTask() {
     var category = catInput.value;
     tasks.push(
         {
+            taskid: Date.now(),
             task: task,
             date: Date.parse(date),
-            category: category
+            category: category,
+            completed: false
         }
     )
     //convert date object to formatted date
     console.log(new Date(date).toLocaleDateString('en-au', { weekday: "long", year: "numeric", month: "short", day: "numeric" }))
 
-    selectTasks('all');
+    findTasks('all');
 }
 
 //this function will create a duplicate task list ready to print to screen based on users needs and order options.
-function selectTasks(type) {
+function findTasks(type) {
+    var list;
     if (type == "all") {
-        //sort from earliest to latest
-        var list = tasks.sort((a, b) => a.date - b.date);
+        //find all non-completed tasks
+        list = tasks.filter(task => task.completed == false);
+    } else if (type == "school") {
+        list = tasks.filter(task => task.category == "School");
+    } else if (type == "personal") {
+        list = tasks.filter(task => task.category == "Personal");
+    } else if (type == "ec") {
+        list = tasks.filter(task => task.category == "EC");
+    } else if (type == "week") {
+        var now = new Date();
+        var endDate = new Date(now.getTime() + 7 * 24 * 60 * 60 * 1000);
+        list = tasks.filter(task =>
+            task.date >= Date.parse(now) &&
+            task.date <= Date.parse(endDate)
+        );
     }
+
+    //last step sort found tasks in order
+    list.sort((a, b) => a.date - b.date);
 
     //send list the to display list function
     showTasks(list);
@@ -44,8 +63,17 @@ function formatDate(date) {
 function showTasks(list) {
     var text = "";
     for (var i = 0; i < list.length; i++) {
-        text += `<div class="box">${formatDate(list[i].date)}<br>${list[i].task}<br>${list[i].category}</div>`;
+        text += `<div class="box ${list[i].category}">${formatDate(list[i].date)}<br>${list[i].task}<br>${list[i].category}<br><button data-id=${list[i].taskid} onclick="completeTask(this)">Complete</button></div>`;
     }
     taskOutput.innerHTML = text;
 }
 
+//this function will get the button that was clicked and set the completed property to true based on the index in the data attribute
+function completeTask(e) {
+    //find the task in the tasks array and update the completed property to true using the found index
+    let task = tasks.find(task => task.taskid == e.dataset.id);
+    if (task) {
+        task.completed = true;
+    }
+    e.parentNode.style.display = 'none';    //hide the element on view
+}
